6a3f4b53f61f45418ae3612657587f40e5c8771e
Hữu Trí <huutrimega@gmail.com>
2021-03-29 21:45:24 +0700

    .

--- travel/src/js/external/index.js
+++ travel/src/js/external/index.js
@@ -13,10 +13,48 @@ const activeTab = (event, tabId) => {
 };
 
 (() => {
-	document.getElementById("scrollToTop").addEventListener("click", () => {
+	const btnToTop = document.getElementById("scrollToTop");
+	window.onscroll = () => {
+		if (window.pageYOffset > screen.height) {
+			btnToTop.classList.remove("hidden");
+		} else {
+			btnToTop.classList.add("hidden");
+		}
+	};
+	btnToTop.addEventListener("click", () => {
 		window.scrollTo({
 			top: 0,
 			behavior: "smooth",
 		});
 	});
 })();
+
+const swiper = new Swiper(".recommend-container", {
+	direction: "horizontal",
+	loop: true,
+	scrollbar: false,
+	speed: 800,
+	slidesPerView: "auto",
+	spaceBetween: 18,
+	centeredSlides: true,
+
+	slideClass: "recommend-slide",
+
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
+
+document.getElementById("hamburger").addEventListener("click", () => {
+	document.getElementById("nav__menu").classList.add("active");
+});
+
+document.getElementById("close-menu").addEventListener("click", () => {
+	document.getElementById("nav__menu").classList.remove("active");
+});
